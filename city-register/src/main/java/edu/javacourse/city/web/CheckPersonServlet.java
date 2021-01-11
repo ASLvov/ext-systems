6ee645e930f80de3d843a28fb968b1c46eaf5bd5
ASLvov <lvov.as@yandex.ru>
2021-01-12 00:24:07 +0500

    get parameters from http request

--- city-register/src/main/java/edu/javacourse/city/web/CheckPersonServlet.java
+++ city-register/src/main/java/edu/javacourse/city/web/CheckPersonServlet.java
@@ -1,11 +1,13 @@
 package edu.javacourse.city.web;
 
 import edu.javacourse.city.dao.PersonCheckDao;
+import edu.javacourse.city.dao.PoolConnectionBuilder;
 import edu.javacourse.city.domain.PersonRequest;
 import edu.javacourse.city.domain.PersonResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @WebServlet(name = "CheckPersonServlet", urlPatterns = {"/checkPerson"})
 public class CheckPersonServlet extends HttpServlet {
@@ -24,23 +27,27 @@ public class CheckPersonServlet extends HttpServlet {
     public void init() throws ServletException {
         logger.info("SERVLET is created");
         dao = new PersonCheckDao();
+        try {
+            dao.setConnectionBuilder(new PoolConnectionBuilder());
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
 
-        String surname = req.getParameter("surname");
-
         PersonRequest personRequest = new PersonRequest();
-        personRequest.setSurName(surname);
-        personRequest.setGivenName("Павел");
-        personRequest.setPatronymic("Николаевич");
-        personRequest.setDateOfBirth(LocalDate.of(1995, 3, 18));
-        personRequest.setStreetCode(1);
-        personRequest.setBuilding("10");
-        personRequest.setExtension("2");
-        personRequest.setApartment("121");
+        personRequest.setSurName(req.getParameter("surname"));
+        personRequest.setGivenName(req.getParameter("givenname"));
+        personRequest.setPatronymic(req.getParameter("patronymic"));
+        LocalDate dateOfBirth = LocalDate.parse(req.getParameter("dateOfBirth"), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        personRequest.setDateOfBirth(dateOfBirth);
+        personRequest.setStreetCode(Integer.parseInt(req.getParameter("streetCode")));
+        personRequest.setBuilding(req.getParameter("building"));
+        personRequest.setExtension(req.getParameter("extension"));
+        personRequest.setApartment(req.getParameter("apartment"));
 
         try {
             PersonResponse ps = dao.checkPerson(personRequest);
