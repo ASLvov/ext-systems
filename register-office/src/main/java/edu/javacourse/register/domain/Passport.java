7ae26f3c1b09bb91796725336b19e3fc2cdf7e50
Александр Львов <Lvov_AS@corp.fcorenburg.com>
2021-01-18 17:25:18 +0500

    add classes for domain model

---
+++ register-office/src/main/java/edu/javacourse/register/domain/Passport.java
@@ -0,0 +1,51 @@
+package edu.javacourse.register.domain;
+
+import java.time.LocalDate;
+
+public class Passport {
+    private Long passportId;
+    private String passportSeria;
+    private String passportNumber;
+    private LocalDate passportIssueDate;
+    private String issueDepartment;
+
+    public Long getPassportId() {
+        return passportId;
+    }
+
+    public void setPassportId(Long passportId) {
+        this.passportId = passportId;
+    }
+
+    public String getPassportSeria() {
+        return passportSeria;
+    }
+
+    public void setPassportSeria(String passportSeria) {
+        this.passportSeria = passportSeria;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public LocalDate getPassportIssueDate() {
+        return passportIssueDate;
+    }
+
+    public void setPassportIssueDate(LocalDate passportIssueDate) {
+        this.passportIssueDate = passportIssueDate;
+    }
+
+    public String getIssueDepartment() {
+        return issueDepartment;
+    }
+
+    public void setIssueDepartment(String issueDepartment) {
+        this.issueDepartment = issueDepartment;
+    }
+}
---
+++ register-office/src/main/java/edu/javacourse/register/domain/PersonFemale.java
@@ -0,0 +1,24 @@
+package edu.javacourse.register.domain;
+
+import java.util.List;
+
+public class PersonFemale extends Person {
+    private List<MarriageCertificate> marriageCetificates;
+    private List<BirthCertificate> birthCetificates;
+
+    public List<MarriageCertificate> getMarriageCetificates() {
+        return marriageCetificates;
+    }
+
+    public void setMarriageCetificates(List<MarriageCertificate> marriageCetificates) {
+        this.marriageCetificates = marriageCetificates;
+    }
+
+    public List<BirthCertificate> getBirthCetificates() {
+        return birthCetificates;
+    }
+
+    public void setBirthCetificates(List<BirthCertificate> birthCetificates) {
+        this.birthCetificates = birthCetificates;
+    }
+}
