d404ca2c30a7acfb58d5c7882220d32e68a00428
Александр Львов <Lvov_AS@corp.fcorenburg.com>
2021-01-19 13:11:24 +0500

    continue describe relations between classes, add unit-test

--- register-office/src/main/java/edu/javacourse/register/domain/PersonMale.java
+++ register-office/src/main/java/edu/javacourse/register/domain/PersonMale.java
@@ -1,24 +1,28 @@
 package edu.javacourse.register.domain;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
 import java.util.List;
 
+@Entity
+@DiscriminatorValue("2")
 public class PersonMale extends Person {
-    private List<MarriageCertificate> marriageCetificates;
-    private List<BirthCertificate> birthCetificates;
-
-    public List<MarriageCertificate> getMarriageCetificates() {
-        return marriageCetificates;
-    }
-
-    public void setMarriageCetificates(List<MarriageCertificate> marriageCetificates) {
-        this.marriageCetificates = marriageCetificates;
-    }
-
-    public List<BirthCertificate> getBirthCetificates() {
-        return birthCetificates;
-    }
-
-    public void setBirthCetificates(List<BirthCertificate> birthCetificates) {
-        this.birthCetificates = birthCetificates;
-    }
+//    private List<MarriageCertificate> marriageCetificates;
+//    private List<BirthCertificate> birthCetificates;
+//
+//    public List<MarriageCertificate> getMarriageCetificates() {
+//        return marriageCetificates;
+//    }
+//
+//    public void setMarriageCetificates(List<MarriageCertificate> marriageCetificates) {
+//        this.marriageCetificates = marriageCetificates;
+//    }
+//
+//    public List<BirthCertificate> getBirthCetificates() {
+//        return birthCetificates;
+//    }
+//
+//    public void setBirthCetificates(List<BirthCertificate> birthCetificates) {
+//        this.birthCetificates = birthCetificates;
+//    }
 }
